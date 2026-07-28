65b088527a009877a2c380dc5bc177861541675f
Taylor <taylor.chavis62@gmail.com>
2026-07-28 16:53:48 -0400

    updated orders.py to remove some relationships

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,22 +1,18 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
-from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+
 from ..dependencies.database import Base
 
 
 class Order(Base):
     __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    order_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
 
-    customer_id = Column(Integer, ForeignKey("customers.id"))
+    customer_id = Column(Integer, ForeignKey("customers.customer_id"), nullable=False)
 
     order_date = Column(DateTime, server_default=func.now())
 
     status = Column(String(50), default="Pending")
 
-    total = Column(Float, nullable=False, default=0)
-
-    customer = relationship("Customer")
-
-    order_details = relationship("OrderDetail", back_populates="order")
+    total_price = Column(Float, nullable=False, default=0)
