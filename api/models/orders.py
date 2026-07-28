b2c1725fc89ba2fd0fc7ba8148d2465191771a78
Taylor <taylor.chavis62@gmail.com>
2026-07-28 16:51:07 -0400

    updated menu_items again to a simpler model

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 from sqlalchemy.orm import relationship
-from datetime import datetime
+from sqlalchemy.sql import func
 from ..dependencies.database import Base
 
 
@@ -8,8 +8,15 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    customer_name = Column(String(100))
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
-    description = Column(String(300))
+
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+
+    order_date = Column(DateTime, server_default=func.now())
+
+    status = Column(String(50), default="Pending")
+
+    total = Column(Float, nullable=False, default=0)
+
+    customer = relationship("Customer")
 
     order_details = relationship("OrderDetail", back_populates="order")
